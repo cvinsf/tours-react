78eeebeace5ab45d683f712884a0bb6dd9bea7b8
Carlos Villarreal <lquilpensl@gmail.com>
2023-11-15 22:17:26 +0000

    created project files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <h2>Tours Starter</h2>
+}
+
+export default App
